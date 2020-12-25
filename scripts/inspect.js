41a293961bde9d3ec7d4dca2d2196810acd6ab06
Cedric van Putten <me@bycedric.com>
2020-12-25 18:44:00 +0100

    fix: use new parser export name in inspect script (#23)

--- scripts/inspect.js
+++ scripts/inspect.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const parse = require('..')
+const { parser } = require('..')
 const inspect = require('unist-util-inspect')
 const { hideBin } = require('yargs/helpers')
 const yargs = require('yargs/yargs')(hideBin(process.argv))
 
 yargs
   .command('$0 <message>', 'Output the parsed syntax tree', () => {}, (argv) => {
-    console.log(inspect(parse(argv.message)))
+    console.log(inspect(parser(argv.message)))
   })
   .parse()
