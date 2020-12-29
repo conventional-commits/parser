99dd6cab10d5ca4d4037092665e49860d0cae51a
bcoe <bencoe@google.com>
2020-12-28 17:48:19 -0800

    feat(conventional-changelog): populate BREAKING CHANGE notes

--- scripts/inspect.js
+++ scripts/inspect.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
-const { parser } = require('..')
+const { parser, toConventionalChangelogFormat } = require('..')
 const inspect = require('unist-util-inspect')
 const { hideBin } = require('yargs/helpers')
 const yargs = require('yargs/yargs')(hideBin(process.argv))
 
 yargs
-  .command('$0 <message>', 'Output the parsed syntax tree', () => {}, (argv) => {
+  .command('$0 <message>', 'output the parsed syntax tree', () => {}, (argv) => {
     console.log(inspect(parser(argv.message)))
+  })
+  .command('cc <message>', 'output conventional changelog format commit', () => {}, (argv) => {
+    const cc = toConventionalChangelogFormat(parser(argv.message))
+    console.log(JSON.stringify(cc, null, 2))
   })
   .parse()
