5ad678567d429174e1ec1eb709a6dd6547f6f2a8
Benjamin E. Coe <bencoe@google.com>
2020-12-29 16:44:15 -0800

    feat(conventional-changelog): handle BREAKING CHANGE in footer and body (#32)

--- test/utils.js
+++ test/utils.js
@@ -17,13 +17,6 @@ describe('utils', () => {
       const parsed = toConventionalChangelogFormat(parser('foo: bar\n\nthe body of commit\nsecond line'))
       assert.strictEqual(parsed.body, 'the body of commit\nsecond line')
     })
-    it('extracts BREAKING CHANGE from header', () => {
-      const parsed = toConventionalChangelogFormat(parser('foo!: hello world'))
-      assert.strictEqual(parsed.notes.length, 1)
-      const note = parsed.notes[0]
-      assert.strictEqual(note.title, 'BREAKING CHANGE')
-      assert.strictEqual(note.text, 'hello world')
-    })
     it('populates references entry from footer', () => {
       const parsed = toConventionalChangelogFormat(parser('foo: summary\n\nRefs #34'))
       assert.strictEqual(parsed.references.length, 1)
@@ -39,6 +32,35 @@ describe('utils', () => {
     it('does not populate reference if it is not numeric', () => {
       const parsed = toConventionalChangelogFormat(parser('foo: summary\n\nRefs #batman'))
       assert.strictEqual(parsed.references.length, 0)
+    })
+    it('extracts BREAKING CHANGE from header', () => {
+      const parsed = toConventionalChangelogFormat(parser('foo!: hello world'))
+      assert.strictEqual(parsed.notes.length, 1)
+      const note = parsed.notes[0]
+      assert.strictEqual(note.title, 'BREAKING CHANGE')
+      assert.strictEqual(note.text, 'hello world')
+    })
+    it('extracts BREAKING CHANGE from body', () => {
+      const parsed = toConventionalChangelogFormat(parser('foo!: hello world\n\nBREAKING CHANGE: this change is breaking\nsecond line'))
+      assert.strictEqual(parsed.notes.length, 1)
+      const note = parsed.notes[0]
+      assert.strictEqual(note.title, 'BREAKING CHANGE')
+      assert.strictEqual(note.text, 'this change is breaking\nsecond line')
+    })
+    it('only extracts text after BREAKING CHANGE token in body', () => {
+      const parsed = toConventionalChangelogFormat(parser('foo!: hello world\n\nstart of body\nBREAKING CHANGE: this change is breaking\nsecond line'))
+      assert.strictEqual(parsed.notes.length, 1)
+      const note = parsed.notes[0]
+      assert.strictEqual(note.title, 'BREAKING CHANGE')
+      assert.strictEqual(note.text, 'this change is breaking\nsecond line')
+    })
+    it('extracts BREAKING CHANGE from footer', () => {
+      const parsed = toConventionalChangelogFormat(parser('foo!: hello world\n\nthis is the body\n\nBREAKING CHANGE: this change is breaking'))
+      assert.strictEqual(parsed.notes.length, 1)
+      assert.strictEqual(parsed.body, 'this is the body')
+      const note = parsed.notes[0]
+      assert.strictEqual(note.title, 'BREAKING CHANGE')
+      assert.strictEqual(note.text, 'this change is breaking')
     })
   })
 })
